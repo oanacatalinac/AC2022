6a5ad04d9e7f7641766b5ecf30b5b4b5246442c4
Cioaba, Oana <oana.cioaba@centric.eu>
2022-04-18 10:29:42 +0300

    First commit

---
+++ ACAutomation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACAutomation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACAutomation")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("808c2168-0641-45ff-9ecf-51d029f5dfc2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ACAutomation/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ACAutomation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void User_Should_Login_Successfully()
+        {
+            //open browser
+            var driver = new ChromeDriver();
+
+            //maximize browser window
+            driver.Manage().Window.Maximize();
+
+            //navigate to url
+            driver.Navigate().GoToUrl("http://a.testaddressbook.com/");
+
+            //click sign in button
+            var btnSignIn = driver.FindElement(By.Id("sign-in"));
+            btnSignIn.Click();
+            Thread.Sleep(2000);
+
+            //fill in email
+            driver.FindElement(By.Id("session_email")).SendKeys("test@test.test");
+
+            //fill in password
+            driver.FindElement(By.XPath("//input[@placeholder='Password']")).SendKeys("test");
+
+            //click sign in button
+            driver.FindElement(By.Name("commit")).Click();
+
+            //assert email
+            Assert.IsTrue(driver.FindElement(By.XPath("//span[@data-test='current-user']")).Text.Equals("test@test.test"));
+
+            //close browser
+            driver.Quit();
+        }
+    }
+}
